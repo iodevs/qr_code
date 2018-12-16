638dfeed5e8d07dfd51cb62409859d1c2e7a59d6
Jindrich K. Smitka <jsmitka@smita.info>
2018-12-16 13:50:46 +0100

    Added dializer settings

--- mix.exs
+++ mix.exs
@@ -4,16 +4,7 @@ defmodule QRCode.MixProject do
   def project do
     [
       app: :qr_code,
-      dialyzer: [
-        plt_add_deps: :transitive,
-        ignore_warnings: "dialyzer.ignore-warnings",
-        flags: [
-          :unmatched_returns,
-          :error_handling,
-          :race_conditions,
-          :no_opaque
-        ]
-      ],
+      dialyzer: dialyzer_base() |> dialyzer_ptl(System.get_env("SEMAPHORE_CACHE_DIR")),
       version: "0.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -53,5 +44,53 @@ defmodule QRCode.MixProject do
       {:xml_builder, "~> 2.1.1"},
       {:csvlixir, "~> 2.0.3"}
     ]
+  end
+
+  defp dialyzer_base() do
+    [
+      plt_add_deps: :transitive,
+      ignore_warnings: "dialyzer.ignore-warnings",
+      flags: [
+        :unmatched_returns,
+        :error_handling,
+        :race_conditions,
+        :no_opaque
+      ]
+    ]
+  end
+
+  defp dialyzer_ptl(base, nil) do
+    base
+  end
+
+  defp dialyzer_ptl(base, path) do
+    base ++
+      [
+        plt_core_path: path,
+        plt_file:
+          Path.join(
+            path,
+            "dialyxir_erlang-#{otp_vsn()}_elixir-#{System.version()}_deps-dev.plt"
+          )
+      ]
+  end
+
+  defp otp_vsn() do
+    major = :erlang.system_info(:otp_release) |> List.to_string()
+    vsn_file = Path.join([:code.root_dir(), "releases", major, "OTP_VERSION"])
+
+    try do
+      {:ok, contents} = File.read(vsn_file)
+      String.split(contents, "\n", trim: true)
+    else
+      [full] ->
+        full
+
+      _ ->
+        major
+    catch
+      :error, _ ->
+        major
+    end
   end
 end
