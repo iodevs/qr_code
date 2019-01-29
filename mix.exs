d2379ec91fbde447494c8192cf5bd78c4594b9e2
ondrej-tucek <ondrej.tucek@gmail.com>
2019-01-29 09:30:48 +0100

    Updated deps packages

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule QRCode.MixProject do
       app: :qr_code,
       dialyzer: dialyzer_base() |> dialyzer_ptl(System.get_env("SEMAPHORE_CACHE_DIR")),
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       description: "Library for generating QR code.",
@@ -35,15 +35,15 @@ defmodule QRCode.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:result, "~> 1.1"},
-      {:ex_maybe, "~> 1.0"},
-      {:ex_doc, "~> 0.18.1", only: :dev},
-      {:credo, "~> 0.9", only: [:dev, :test]},
-      {:excoveralls, "~> 0.9", only: :test},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:result, "~> 1.3.0"},
+      {:ex_maybe, "~> 1.0.1"},
+      {:ex_doc, "~> 0.19.3", only: :dev},
+      {:credo, "~> 1.0.1", only: [:dev, :test]},
+      {:excoveralls, "~> 0.10.4", only: :test},
+      {:dialyxir, "~> 0.5.1", only: [:dev], runtime: false},
       {:xml_builder, "~> 2.1.1"},
-      {:csvlixir, "~> 2.0.3"},
-      {:matrix_reloaded, "~> 2.0.0"}
+      {:csvlixir, "~> 2.0.4"},
+      {:matrix_reloaded, "~> 2.0.1"}
     ]
   end
 
