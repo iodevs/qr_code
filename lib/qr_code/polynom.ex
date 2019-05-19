58589cd4dccb645dfd605a02cbd1f350dbca3f25
Jindrich K. Smitka <jsmitka@smita.info>
2019-05-19 11:44:28 +0200

    Fixed #8 -  polynom division

--- lib/qr_code/polynom.ex
+++ lib/qr_code/polynom.ex
@@ -19,12 +19,33 @@ defmodule QRCode.Polynom do
     multipled_divisor =
       Enum.map(divisor, fn val -> val |> GF.add(GF.to_a(first)) |> GF.to_i() end)
 
-    [0 | result] = xor(dividend, multipled_divisor)
-    div(result, divisor, step - 1)
+    {result, step} =
+      dividend
+      |> xor(multipled_divisor)
+      |> trim_leading_zero(step)
+      |> fill_to_degree(Enum.count(divisor) - 1)
+
+    div(result, divisor, step)
   end
 
   defp xor(dividend, divisor, acc \\ [])
   defp xor([], [], acc), do: Enum.reverse(acc)
   defp xor([], [b | divisor], acc), do: xor([], divisor, [b | acc])
   defp xor([a | dividend], [b | divisor], acc), do: xor(dividend, divisor, [a ^^^ b | acc])
+
+  defp trim_leading_zero([0 | list], step) do
+    trim_leading_zero(list, step - 1)
+  end
+
+  defp trim_leading_zero(list, step) do
+    {list, step}
+  end
+
+  defp fill_to_degree({list, step}, degree) when length(list) < degree do
+    {list ++ List.duplicate(0, degree - Enum.count(list)), step}
+  end
+
+  defp fill_to_degree(result, _) do
+    result
+  end
 end
