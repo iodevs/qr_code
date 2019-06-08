ae9225704f54515f787c765e974b91274a699c29
Jindrich K. Smitka <jsmitka@smita.info>
2019-06-08 12:21:59 +0200

    Added find version test

---
+++ test/data_encoding_test.exs
@@ -0,0 +1,19 @@
+defmodule DataEncodingTest do
+  @moduledoc false
+  use ExUnit.Case
+  use PropCheck
+  doctest QRCode.DataEncoding
+
+  alias QRCode.DataEncoding
+
+  # Tests
+  test "my test description" do
+    assert false
+  end
+
+  # Properties
+
+  # Helpers
+
+  # Generators
+end
