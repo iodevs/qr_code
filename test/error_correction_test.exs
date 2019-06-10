8d4b948b2da084ccffccaaf4b5700eacea852582
Jindrich K. Smitka <jsmitka@smita.info>
2019-06-10 12:00:06 +0200

    Added error correction test skeleton

---
+++ test/error_correction_test.exs
@@ -0,0 +1,20 @@
+defmodule ErrorCorrectionTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+  use PropCheck
+  doctest QRCode.ErrorCorrection
+
+  alias QRCode.ErrorCorrection
+
+  # Tests
+
+  test "my test description" do
+    assert false
+  end
+
+  # Properties
+
+  # Helpers
+
+  # Generators
+end
