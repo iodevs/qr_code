2ddc34a596acfbf257e4b9e2d37803f7fb24a0da
Ondrej Tucek <ondrej.tucek@gmail.com>
2020-12-30 14:10:05 +0100

    Fix guard for encode_color function

--- lib/qr_code/svg.ex
+++ lib/qr_code/svg.ex
@@ -202,7 +202,7 @@ defmodule QRCode.Svg do
     color
   end
 
-  def encode_color(c) when is_integer(c) and c in [0, 255] do
+  def encode_color(c) when is_integer(c) and 0 <= c and c <= 255 do
     c |> :binary.encode_unsigned() |> Base.encode16()
   end
 end
