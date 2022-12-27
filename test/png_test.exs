83df7ab712433ca9ae6096dca24b181a7db72a8e
Ondrej Tucek <ondrej.tucek@gmail.com>
2022-12-27 18:46:05 +0100

    Add Png test

---
+++ test/png_test.exs
@@ -0,0 +1,56 @@
+defmodule PngTest do
+  @moduledoc false
+
+  use ExUnit.Case
+  alias QRCode
+  alias QRCode.Render.PngSettings
+
+  @text "HELLO WORLD"
+  @dst_to_file "/tmp/hello.png"
+
+  describe "Png" do
+    setup do
+      @text
+      |> QRCode.create()
+      |> QRCode.render(:png)
+      |> QRCode.save(@dst_to_file)
+
+      on_exit(fn ->
+        :ok = File.rm(@dst_to_file)
+      end)
+    end
+
+    test "should save qr code to png file" do
+      assert File.exists?(@dst_to_file)
+    end
+
+    test "should create png from qr matrix" do
+      {:ok, expected} =
+        @text
+        |> QRCode.create()
+        |> QRCode.render(:png)
+
+      rv =
+        @dst_to_file
+        |> File.read!()
+
+      assert expected == rv
+    end
+
+    test "should encoded png binary to base64" do
+      {:ok, expected} =
+        @text
+        |> QRCode.create()
+        |> QRCode.render(:png)
+
+      {:ok, rv} =
+        @text
+        |> QRCode.create()
+        |> QRCode.render(:png)
+        |> QRCode.to_base64()
+        |> Result.and_then(&Base.decode64/1)
+
+      assert expected == rv
+    end
+  end
+end
