777882bf8e2f405248e3680c4004c8efa2136d2c
Jindrich K. Smitka <jsmitka@smita.info>
2023-01-07 15:01:35 +0100

    Moved save and to_base64 functions to better place

--- lib/qr_code.ex
+++ lib/qr_code.ex
@@ -26,57 +26,14 @@ defmodule QRCode do
     to: QRCode.Render
 
   @doc """
-  Encodes rendered QR matrix as `svg` or `png` binary into a base 64.
-  This encoded string can be then used in Html as
-
-  For `svg` use
-  ```
-  <img src="data:image/svg+xml; base64, encoded_svg_qr_code" alt="QR code" />
-  ```
-
-  and in case of `png`
-  ```
-  <img src="data:image/png; base64, encoded_png_qr_code" alt="QR code" />
-  ```
+  See `QRCode.Render.to_base64/1`
   """
-  @spec to_base64(Result.t(String.t(), binary())) :: Result.t(String.t(), binary())
-  def to_base64({:ok, rendered_qr_matrix}) do
-    rendered_qr_matrix
-    |> Base.encode64()
-    |> Result.ok()
-  end
-
-  def to_base64(error), do: error
+  defdelegate to_base64(qr_result),
+    to: QRCode.Render
 
   @doc """
-  Saves rendered QR code to `svg` or `png` file. See a few examples below:
-
-  ```elixir
-    iex> "Hello World"
-          |> QRCode.create(:high)
-          |> QRCode.render(:svg)
-          |> QRCode.save("/path/to/hello.svg")
-    {:ok, "/path/to/hello.svg"}
-  ```
-
-  ```elixir
-    iex> png_settings = %QRCode.Render.PngSettings{qrcode_color: {17, 170, 136}}
-    iex> "Hello World"
-          |> QRCode.create(:high)
-          |> QRCode.render(:png, png_settings)
-          |> QRCode.save("/tmp/to/hello.png")
-    {:ok, "/path/to/hello.png"}
-  ```
-  ![QR code color](docs/qrcode_color.png)
+  See `QRCode.Render.save/2`
   """
-  @spec save(Result.t(any(), binary()), Path.t()) ::
-          Result.t(String.t() | File.posix() | :badarg | :terminated, Path.t())
-  def save({:ok, rendered_qr_matrix}, path_with_file_name) do
-    case File.write(path_with_file_name, rendered_qr_matrix) do
-      :ok -> {:ok, path_with_file_name}
-      err -> err
-    end
-  end
-
-  def save(error, _path_with_file_name), do: error
+  defdelegate save(qr_result, path_with_file_name),
+    to: QRCode.Render
 end
